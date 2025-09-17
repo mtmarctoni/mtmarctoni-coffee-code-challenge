abb03010d66f977a8ddb09d95c9c08862394b661
mtmarctoni <marctonimas@protonmail.com>
2025-09-17 11:52:25 +0200

    fix: add checks for docker-compose command availability

--- backend/scripts/start-dev-db.sh
+++ backend/scripts/start-dev-db.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
-docker-compose -f db/development_db.yml up -d
+if command -v docker-compose &> /dev/null; then
+  docker-compose -f db/development_db.yml up -d
+elif docker compose &> /dev/null; then
+  docker compose -f db/development_db.yml up -d
+else
+  echo "Error: Neither 'docker-compose' nor 'docker compose' commands are available."
+  exit 1
+fi
 
